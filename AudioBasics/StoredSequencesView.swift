590cf24c00a4e42898bb7dbe5bea282609d6853b
sypianBS <sypianbs@gmail.com>
2022-03-05 18:39:39 +0100

    remove chosen sequence

--- AudioBasics/StoredSequencesView.swift
+++ AudioBasics/StoredSequencesView.swift
@@ -9,16 +9,29 @@ import Foundation
 import SwiftUI
 
 struct StoredSequencesView: View {
+    @State var selectedEntry = ""
     @Binding var notesSequenceDictionary: [String : [Float]]
     
     var body: some View {
         VStack {
             List {
                 ForEach(notesSequenceDictionary.keys.sorted(), id: \.self) { key in
-                    Text(key.description)
-                }
+                    Text(key)
+                        .onTapGesture {
+                            selectedEntry = key
+                        }
+                }.onDelete(perform: removeRows)
             }
+        }.toolbar {
+            EditButton()
         }
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        let sortedKeys = notesSequenceDictionary.keys.sorted()
+        let index = offsets[offsets.startIndex]
+        notesSequenceDictionary[sortedKeys[index]] = nil
+        UserDefaults.standard.set(notesSequenceDictionary, forKey: UtilStrings.keyStoredSequence)
     }
 }
 
