fac2ab177385372c3fd0b01ab2135d303c44fbba
sypianBS <sypianbs@gmail.com>
2022-03-06 11:46:48 +0100

    organize the project structure

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 This is an early version of a step sequencer which currently plays random notes sequences. The ultimate goal is to make it fully functional by providing a proper UI with an ability to play user-chosen notes sequences.
 Author: sypianBS bensypianski@googlemail.com
 
+![simulator_screenshot_E6EA2303-0EBE-4BB2-83F7-D73B022D99D8](https://user-images.githubusercontent.com/99125193/156919405-b8a796d4-7b4f-4be7-9db5-94788329488b.png)
+
 Current features:
 - 3 waveforms (sine, saw, square)
 - 5 different rates (1, 1/2, 1/4, 1/8, 1/16)
--- TDStepSequencer/SoundGeneration/Oscillator.swift
+++ TDStepSequencer/SoundGeneration/Oscillator.swift
@@ -19,7 +19,7 @@ struct Oscillator {
     
     ///sine is a periodic function of time with a period equal to (2 * pi) over b, where b is the factor that time or x is being multiplied by before being passed into the function. In our case, b is equal to (2 * pi * Oscillator.frequency). That means the period of our sine wave is (1 / Oscillator.frequency). This makes perfect sense because our frequency is in Hz or cycles per second. If there are 440 cycles per second in a sine wave, each cycle is allotted 1 / 440th of a second.
     static let sine: Signal =  { frequency, time in
-        return Oscillator.amplitude * sin(2.0 * Float.pi * frequency * time) //todoben where does the formula come form ?
+        return Oscillator.amplitude * sin(2.0 * Float.pi * frequency * time)
     }
     
     static let saw: Signal = { frequency, time in
