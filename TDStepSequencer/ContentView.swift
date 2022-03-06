8ba4fd729bd3b09858b15a1bd22b7d1ea57af5a5
sypianBS <sypianbs@gmail.com>
2022-03-06 11:27:21 +0100

    clean the ContentView

--- TDStepSequencer/ContentView.swift
+++ TDStepSequencer/ContentView.swift
@@ -8,89 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    typealias Pitch = SoundEngine.Pitch
+    typealias SequencerRate = SequencerViewModel.SequencerRate
     @StateObject var sequencerViewModel = SequencerViewModel()
-    @State var playSound = false
     @State var selectedPitch: Float? = nil
     @State private var bpm = 120
     @State var showSequencesList = false
-    let thirdOctave = Octave(octaveNumber: 3)
     @State var selectedRate: SequencerRate = .eight
     @State var selectedWaveform: Oscillator.Waveform = .saw
     @State var selectedEntry: [Float] = []
     @State var isPlaying = false
-    typealias Pitch = SoundEngine.Pitch
-    typealias SequencerRate = SequencerViewModel.SequencerRate
     let assetsSize: CGFloat = 24
     
     var body: some View {
         NavigationView {
             VStack(alignment: .leading, spacing: 24) {
-                VStack(alignment: .leading) {
-                    Text("Playback settings".uppercased())
-                        .font(.headline)
-                    VStack {
-                        Stepper(UtilStrings.bpm +  ": \(bpm)", value: $bpm, in: 20...200)
-                        Picker(selection: $selectedWaveform, label: Text("Rate")) {
-                            ForEach(Oscillator.Waveform.allCases, id: \.self) {
-                                Text($0.rawValue)
-                            }
-                        }.pickerStyle(SegmentedPickerStyle())
-                        
-                        Picker(selection: $selectedRate, label: Text("Rate")) {
-                            ForEach(SequencerRate.allCases, id: \.self) {
-                                Text($0.description)
-                            }
-                        }.pickerStyle(SegmentedPickerStyle())
-                    }.padding(.horizontal, 8)
-                }
-                
-                VStack(alignment: .leading) {
-                    Text("Play, load, save".uppercased())
-                        .font(.headline)
-                        .padding(.bottom, 8)
-                    HStack(spacing: 16) {
-                        Spacer()
-                        Button(action: {
-                            if !isPlaying {
-                                selectedEntry = []
-                                sequencerViewModel.generateARandomTenNotesSequence()
-                                //selectedEntry = []
-                                isPlaying = true
-                            } else {
-                                sequencerViewModel.stopPlaying()
-                                selectedEntry = []
-                                isPlaying = false
-                            }
-                        }, label: {
-                            Image(systemName: isPlaying ? "playpause.fill" : "playpause")
-                                .font(.system(size: assetsSize))
-                        })
-                        
-                        if sequencerViewModel.notesSequenceDictionary.count > 0 {
-                            Button(action: {
-                                showSequencesList = true
-                            }, label: {
-                                Image(systemName: "list.dash")
-                                    .font(.system(size: assetsSize))
-                            })
-                        } else {
-                            Image(systemName: "list.dash")
-                                .font(.system(size: assetsSize))
-                                .foregroundColor(.gray)
-                        }
-                        
-                        Button(action: { sequencerViewModel.storeSequence() }, label: {
-                            Image(systemName: "square.and.arrow.down")
-                                .font(.system(size: assetsSize))
-                                .offset(y: -3)
-                        })
-                        Spacer()
-                    }
-                    NavigationLink("", isActive: $showSequencesList) {
-                        StoredSequencesView(showView: $showSequencesList, selectedEntry: $selectedEntry,  notesSequenceDictionary: $sequencerViewModel.notesSequenceDictionary)
-                    }
-                   
-                }
+                playbackSettingsView
+                playLoadSaveView
             }
             .padding(.horizontal, 16)
             .onChange(of: selectedWaveform, perform: {
@@ -104,13 +38,83 @@ struct ContentView: View {
                 newBPM in
                 sequencerViewModel.setBpm(bpm: newBPM)
             }).onChange(of: selectedEntry, perform: {
-                loadedENtry in                
+                _ in
                 if selectedEntry.count > 0 {
                     sequencerViewModel.playLoadedSequence(sequence: selectedEntry)
                     isPlaying = true
                 }
             })
         }
+    }
+    
+    var playbackSettingsView: AnyView {
+        return AnyView(VStack(alignment: .leading) {
+            Text("Playback settings".uppercased())
+                .font(.headline)
+            VStack {
+                Stepper(UtilStrings.bpm +  ": \(bpm)", value: $bpm, in: 20...200)
+                Picker(selection: $selectedWaveform, label: Text("Rate")) {
+                    ForEach(Oscillator.Waveform.allCases, id: \.self) {
+                        Text($0.rawValue)
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+                
+                Picker(selection: $selectedRate, label: Text("Rate")) {
+                    ForEach(SequencerRate.allCases, id: \.self) {
+                        Text($0.description)
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+            }.padding(.horizontal, 8)
+        })
+    }
+    
+    var playLoadSaveView: AnyView {
+        return AnyView(VStack(alignment: .leading) {
+            Text("Play, load, save".uppercased())
+                .font(.headline)
+                .padding(.bottom, 8)
+            HStack(spacing: 16) {
+                Spacer()
+                Button(action: {
+                    if !isPlaying {
+                        selectedEntry = []
+                        sequencerViewModel.generateARandomTenNotesSequence()
+                        isPlaying = true
+                    } else {
+                        sequencerViewModel.stopPlaying()
+                        selectedEntry = []
+                        isPlaying = false
+                    }
+                }, label: {
+                    Image(systemName: isPlaying ? "playpause.fill" : "playpause")
+                        .font(.system(size: assetsSize))
+                })
+                
+                if sequencerViewModel.notesSequenceDictionary.count > 0 {
+                    Button(action: {
+                        showSequencesList = true
+                    }, label: {
+                        Image(systemName: "list.dash")
+                            .font(.system(size: assetsSize))
+                    })
+                } else {
+                    Image(systemName: "list.dash")
+                        .font(.system(size: assetsSize))
+                        .foregroundColor(.gray)
+                }
+                
+                Button(action: { sequencerViewModel.storeSequence() }, label: {
+                    Image(systemName: "square.and.arrow.down")
+                        .font(.system(size: assetsSize))
+                        .offset(y: -3)
+                })
+                Spacer()
+            }
+            NavigationLink("", isActive: $showSequencesList) {
+                StoredSequencesView(showView: $showSequencesList, selectedEntry: $selectedEntry,  notesSequenceDictionary: $sequencerViewModel.notesSequenceDictionary)
+            }
+           
+        })
     }
 }
 
